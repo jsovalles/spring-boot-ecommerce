24e8dc117ccee9ced4b24d2cb0304c6340852be7
jsovalles <jsovalles95@gmail.com>
2020-08-10 18:37:35 -0500

    adding starter files for 2.0 and creating a method to search products by category

--- src/main/java/com/ecommerce/dao/IProductDAO.java
+++ src/main/java/com/ecommerce/dao/IProductDAO.java
@@ -2,9 +2,14 @@ package com.ecommerce.dao;
 
 
 import com.ecommerce.dao.entity.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @CrossOrigin("http://localhost:4200")
 public interface IProductDAO extends JpaRepository<Product, Integer> {
+
+    Page<Product> findByCategoryId(@RequestParam("id") int id, Pageable pageable);
 }
