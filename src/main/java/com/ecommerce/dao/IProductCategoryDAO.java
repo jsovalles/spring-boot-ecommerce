e54d43c0dd43e1baba87f5ab9b69233a2787160a
jsovalles <jsovalles95@gmail.com>
2020-08-18 16:23:20 -0500

    searching products by dynamic search

--- src/main/java/com/ecommerce/dao/IProductCategoryDAO.java
+++ src/main/java/com/ecommerce/dao/IProductCategoryDAO.java
@@ -2,8 +2,10 @@ package com.ecommerce.dao;
 
 import com.ecommerce.dao.entity.ProductCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 @CrossOrigin("http://localhost:4200")
+@RepositoryRestResource(collectionResourceRel = "productCategory", path = "product-category")
 public interface IProductCategoryDAO extends JpaRepository<ProductCategory, Integer> {
 }
